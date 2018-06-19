407d113f6bf649bb775f2526aa32ca01ab5c7db2
Pedro Fumero <mail@pedrofumero.com>
2018-06-18 21:47:09 -0400

    Agregando vistas de creación de producto y modificación

--- resources/views/producto/producto-create.blade.php
+++ resources/views/producto/producto-create.blade.php
@@ -1,9 +1,8 @@
-@extends('producto.productos')
+{{-- @extends('producto.productos')
 @section('central')
 
 
     <form class="form-group" action="/productos" method="POST" enctype="multipart/form-data">
-          {{-- se coloca siempre por protecccion de hackers en laravel--}}
           @csrf
 
           <div class="container">
@@ -35,7 +34,67 @@
           </div>
       </form>
 
-
-
-
   @stop
+
+--}}
+
+
+
+@extends('layouts.master')
+
+@section('title', 'Crear producto')
+
+@section('contenido')
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="titulos-principales">Crear productos</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-offset-4 col-md-4">
+
+        <div class="modal-body">
+
+              <div class="form-group">
+                <input type="text" name="p_cod"  class ="form-control"  placeholder="Código del producto" required>
+              </div>
+               <div class="form-group">
+                   <input type="text" name="p_nombre" required class="form-control input-lg" placeholder="Nombre del producto"/>
+               </div>
+
+               <div class="form-group">
+                      <input type="text" name="p_tipo"  class= "form-control" required placeholder="Tipo">
+                </div>
+
+               <div class="form-group">
+                   <input type="text" class="form-control input-lg" placeholder="Cantidad"/>
+               </div>
+               <div class="form-group">
+                   <input type="text" class="form-control input-lg" placeholder="Precio"/>
+               </div>
+
+               <div class="form-group">
+                      <input type="file" name= "p_imagen"  placeholder="Imagen del producto">
+                  </div>
+
+               <div class="form-group">
+                   <textarea class="form-control input-lg" placeholder="Descripción del producto"></textarea> 
+               </div>
+
+               <div class="form-group">
+                   <input type="submit" class="btn btn-block btn-lg btn-primary" value="Crear producto"/>
+               </div>
+           </div>
+
+      </div>
+    </div>
+  </div>
+        
+
+        
+
+
+@stop
--- resources/views/producto/productos-edit.blade.php
+++ resources/views/producto/productos-edit.blade.php
@@ -1,11 +1,11 @@
-@extends('producto.productos')
+{{--@extends('producto.productos')
 @section('central')
 
 
     <form class="form-group" action="/productos/{{$producto->p_cod}}" method="POST" enctype="multipart/form-data">
-      {{-- se coloca paraindicarque vamos acualizar--}}
+     
           @method('PUT')
-          {{-- se coloca siempre por protecccion de hackers en laravel--}}
+          
           @csrf
 
           <div class="container">
@@ -27,7 +27,7 @@
 
                   <div class="form-group">
                   <img class="estilo-tiendas" src="{{asset('insertado/producto/'.$producto->p_imagen)}}" alt="">
-                    </div>
+                  </div>
 
                     <div class="form-group">
                         <label>imagen</label>
@@ -41,3 +41,78 @@
 
 
   @stop
+
+
+
+--}}
+
+
+@extends('layouts.master')
+
+@section('title', 'Modificar producto')
+
+@section('contenido')
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="titulos-principales">Modificar producto</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-offset-4 col-md-4">
+
+        <p>Mostrando N productos</p>
+
+        <select style="width: 90%;text-align: center; margin: 0 auto" class="form-control" id="sel">
+        <option>Seleccione el producto</option>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        </select>
+            
+          
+
+        <div class="modal-body">
+               <div class="form-group">
+                   <input type="text" class="form-control input-lg" placeholder="Nombre del producto" name="p_nombre" value="{{$producto->p_nombre}}" required/>
+               </div>
+               <div class="form-group">
+                 <input type="text" name="p_tipo"  class= "form-control" value="{{$producto->p_tipo}}" required placeholder="Tipo">
+               </div>
+               <div class="form-group">
+                   <input type="text" class="form-control input-lg" placeholder="Cantidad"/>
+               </div>
+               <div class="form-group">
+                   <input name="p_precio"  value="{{$producto->p_precio}}" type="text" class="form-control input-lg" placeholder="Precio" required/>
+               </div>
+
+               <div class="form-group">
+                   <textarea class="form-control input-lg" placeholder="Descripción del producto"></textarea> 
+               </div>
+
+               <div class="form-group">
+                  <img class="estilo-tiendas" src="{{asset('insertado/producto/'.$producto->p_imagen)}}" alt="">
+                  </div>
+
+                    <div class="form-group">
+                        <label>imagen</label>
+                        <input type="file" name= "p_imagen" value="{{$producto->p_imagen}}" >
+                    </div>
+
+               <div class="form-group">
+                   <input type="submit" class="btn btn-block btn-lg btn-primary" value="Modificar producto"/>
+               </div>
+           </div>
+
+      </div>
+    </div>
+  </div>
+        
+
+        
+
+
+@stop
