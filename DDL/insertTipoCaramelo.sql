0293314754a2f26ad7d2f05f0a58ada0b161a8ae
Pedro Fumero <mail@pedrofumero.com>
2018-06-21 23:45:20 -0400

    Agregando inserts de Diario, Salario, Empleado, Tipo de caramelo

---
+++ DDL/insertTipoCaramelo.sql
@@ -0,0 +1,16 @@
+insert into TIPO_CARAMELO(c_cod,c_nombre,c_descripcion,FK_producto) values
+	(001,'Normal','',7),
+	(002,'Macizo','',2),
+	(003,'Con chicle', '',6),
+	(004,'Piruleta','',4),
+	(005,'Bombón','',3),
+	(006,'Gelatina','',6),
+	(007,'Algodón de azúcar','',5),
+	(008,'Con chocolate','',10),
+	(009,'De fruta','',2),
+	(010,'De regaliz','',8),
+	(011,'Sin azúcar','',1),
+	(012,'Picantes','',4),
+	(013,'Ácidos','',10),
+	(014,'Con colorante','',4);
+
